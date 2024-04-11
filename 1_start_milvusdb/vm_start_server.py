4e23d93b737d30446374d8a46cdced4b9f196c07
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-11 07:47:27 -0500

    fixes

--- 1_start_milvusdb/vm_start_server.py
+++ 1_start_milvusdb/vm_start_server.py
@@ -1,6 +1,21 @@
-import subprocess
+from milvus import default_server
+from pymilvus import connections, Collection, utility
+import multiprocessing
+import time
+ 
+def start_milvus():
+    try:
+        default_server.set_base_dir('milvus-data')
+        default_server.start()
+    except Exception as e:
+        default_server.stop()
+        raise (e)
 
-print(subprocess.run(["sh 1_start_milvusdb/vm_start_server.sh"], shell=True))
+if __name__ == "__main__":
+    p = multiprocessing.Process(target=start_milvus, daemon=True)
+    p.start()
+    print("Started MilvusDB server")
+    time.sleep(60)
 
 #auth=os.environ.get('MILVUS_AUTH', 'false').lower()
 #if auth!='false' and auth!='true':
