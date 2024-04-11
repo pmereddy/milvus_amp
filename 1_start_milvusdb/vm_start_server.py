329aaf855ff4f3ebfcdec9766c02f809aee9ca82
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-10 21:52:55 -0500

    fix

--- 1_start_milvusdb/vm_start_server.py
+++ 1_start_milvusdb/vm_start_server.py
@@ -5,4 +5,4 @@ auth=os.environ.get('MILVUS_AUTH', 'false').lower()
 if auth!='false' and auth!='true':
     auth='false'
 
-subprocess.run(["milvus-server --data $HOME/milvus-data --authorization-enabled " + str(auth)], shell=False)
+subprocess.run(["$HOME/.local/bin/milvus-servermilvus-server --data $HOME/milvus-data --authorization-enabled " + str(auth)], shell=False)
