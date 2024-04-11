56246a152ab51c0b13d84c381c3cf34ab51478bd
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-10 23:17:15 -0500

    fixes

--- 1_start_milvusdb/vm_start_server.py
+++ 1_start_milvusdb/vm_start_server.py
@@ -5,9 +5,8 @@ auth=os.environ.get('MILVUS_AUTH', 'false').lower()
 if auth!='false' and auth!='true':
     auth='false'
 
-command = ["/usr/bin/nohup", "$HOME/.local/bin/milvus-server", "--data $HOME/milvus-data", "--authorization-enabled", auth, " & "]
+#command = ["/usr/bin/nohup", "$HOME/.local/bin/milvus-server", "--data $HOME/milvus-data", "--authorization-enabled", auth, " & "]
+command=f"/usr/bin/nohup $HOME/.local/bin/milvus-server --data $HOME/milvus-data --authorization-enabled {auth} & "
 
-process = subprocess.Popen(command, 
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=False)
-
-print(process.pid)
+rc=os.system(command)
+print(rc)
