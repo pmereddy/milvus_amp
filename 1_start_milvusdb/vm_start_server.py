c740b5aab0426f93790fa64072dbe941dcfd063b
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-11 08:47:52 -0500

    fixes

--- 1_start_milvusdb/vm_start_server.py
+++ 1_start_milvusdb/vm_start_server.py
@@ -11,18 +11,19 @@ def start_milvus():
         default_server.stop()
         raise (e)
 
+def start_milvus2():
+    auth=os.environ.get('MILVUS_AUTH', 'false').lower()
+    if auth!='false' and auth!='true':
+        auth='false'
+    command = ["/usr/bin/nohup", "$HOME/.local/bin/milvus-server", "--data $HOME/milvus-data", "--authorization-enabled", auth, " & "]
+    #command=f"/usr/bin/nohup $HOME/.local/bin/milvus-server --data $HOME/milvus-data --authorization-enabled {auth} & "
+    rc=os.system(command)
+    print(rc)
+
 if __name__ == "__main__":
-    p = multiprocessing.Process(target=start_milvus, daemon=True)
+    p = multiprocessing.Process(target=start_milvus2, daemon=True)
     p.start()
     print("Started MilvusDB server")
-    time.sleep(60)
+    time.sleep(5)
 
-#auth=os.environ.get('MILVUS_AUTH', 'false').lower()
-#if auth!='false' and auth!='true':
-#    auth='false'
 
-#command = ["/usr/bin/nohup", "$HOME/.local/bin/milvus-server", "--data $HOME/milvus-data", "--authorization-enabled", auth, " & "]
-#command=f"/usr/bin/nohup $HOME/.local/bin/milvus-server --data $HOME/milvus-data --authorization-enabled {auth} & "
-
-#rc=os.system(command)
-#print(rc)
