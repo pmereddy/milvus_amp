516a4a5a6635af5b19e7198da151ec08d7ffefb0
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-12 00:13:35 -0500

    fixes

--- 1_start_milvusdb/vm_start_server.py
+++ 1_start_milvusdb/vm_start_server.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
- 
+import sys
+
 print(subprocess.run(["1_start_milvusdb/vm_start_server.sh"], shell=True))
 time.sleep(15)
-# ! /home/cdsw/.local/bin/milvus-server --debug --data $HOME/milvus-data --authorization-enabled true > /dev/null 2>&1 &
+sys.exit(0)
--- 1_start_milvusdb/vm_start_server.sh
+++ 1_start_milvusdb/vm_start_server.sh
@@ -2,6 +2,7 @@
 
 rm -rf $HOME/milvus-data
 nohup $HOME/.local/bin/milvus-server --debug --data $HOME/milvus-data --authorization-enabled true >/dev/null 2>&1 &
+ 
 pid="$!"
 disown -a $pid
 exit 0
