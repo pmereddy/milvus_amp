bf4238242c4c8fba81d6c8b5c15165702fc3e24a
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-11 23:27:34 -0500

    fixes

--- 1_start_milvusdb/vm_start_server.sh
+++ 1_start_milvusdb/vm_start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf $HOME/milvus-data
-nohup $HOME/.local/bin/milvus-server --data $HOME/milvus-data --authorization-enabled true >/dev/null 2>&1 &
+nohup $HOME/.local/bin/milvus-server --debug --data $HOME/milvus-data --authorization-enabled true >/dev/null 2>&1 &
 pid="$!"
 disown -a $pid
 exit 0
